590755c0f03bce06be8169c0748c88c36490e7f4
Kamesh Mishra <58304583+Kamesh-Mishra@users.noreply.github.com>
2024-01-01 22:19:31 +0530

    Add files via upload

---
+++ MyFirst.java
@@ -0,0 +1,41 @@
+package myfirst;
+
+//   control + shift  +  L     for keyboard shortcuts
+
+
+public class MyFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		int x = 5, y;
+//		
+//		y = x++;
+//		
+//		
+//		System.out.println( "  y = x++ = " + y + ",,  x = " + x  );
+//		
+//		int a = 5, b;
+//		b = ++a;
+//		
+//		System.out.println(" b  = ++a = " + b + ",  a = " + a);
+		
+		
+//		
+//		int a = 2, b = 5, x = 4, c;
+//		c = a * x++ + b;
+//		System.out.println("c = " + c + ",  x = " + x );
+
+//		int a = 2, b = 5, x = 4, c;
+//		c = a * ++x + b;
+//		System.out.println("c = " + c + ",  x = " + x );
+		
+		int x = 10, y = 20;
+		
+		System.out.println();
+		
+		
+	}
+
+}
